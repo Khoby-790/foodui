44e19a75505f0975cb9cf39e2dd0898a8a732e4f
Khoby-790 <ebaidoo79@gmail.com>
2021-01-04 09:01:27 +0800

    Food UI Test

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const System = () => (
+    <NavigationContainer>
+        <App />
+    </NavigationContainer>
+)
+
+AppRegistry.registerComponent(appName, () => System);
---
+++ navigation/DrawerNavigation.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+
+const DrawerNavigation = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default DrawerNavigation
